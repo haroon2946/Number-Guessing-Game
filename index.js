5021af78321c0f38f54103c3581d9f389854f9b1
DELL <afridi2542@gmail.com>
2024-03-24 21:52:48 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+// computer will generate random numbers
+// user input for guessing number
+// compare user input with computer generated numbers
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Please guess number between 1-10; ",
+    },
+]);
+if (answers.userGuessedNumber === randomNumber) {
+    console.log("Congrualtions! you guessed right number.");
+}
+else {
+    console.log("You guessed wrong number");
+}
